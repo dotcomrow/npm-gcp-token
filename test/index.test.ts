78604caa8f9ddb50d66bd02f77901ef76e43979f
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-08 17:41:45 -0400

    updating test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,9 @@
 import { GCPAccessToken } from '../src/index.js';
 import * as fs from 'fs';
 
-const keyFilePath = '/Users/admin/Downloads/logging-test-key.json'; // replace with the path to your service account key file
+const keyFilePath = '/Users/admin/Downloads/org-service-accounts-401323-6ec6fc1915ca.json'; // replace with the path to your service account key file
 const keyFileContent = fs.readFileSync(keyFilePath, 'utf8');
 
 var accessToken = new GCPAccessToken(keyFileContent);
-var token = await accessToken.getAccessToken('https://www.googleapis.com/auth/logging.write');
+var token = await accessToken.getAccessToken('email');
 console.log(token);
